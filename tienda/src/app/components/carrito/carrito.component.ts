5983fbc017e44d27112ac9253792683dd0cb4c0b
FraTzu <fratzu.15@gmail.com>
2022-10-02 01:57:15 -0500

    avance 1era iteracion

---
+++ tienda/src/app/components/carrito/carrito.component.ts
@@ -0,0 +1,80 @@
+import { Component, OnInit } from '@angular/core';
+import { GLOBAL } from 'src/app/services/GLOBAL';
+import { ClienteService } from 'src/app/services/cliente.service';
+import { io } from "socket.io-client";
+
+declare var iziToast:any;
+
+@Component({
+  selector: 'app-carrito',
+  templateUrl: './carrito.component.html',
+  styleUrls: ['./carrito.component.css']
+})
+export class CarritoComponent implements OnInit {
+
+  public idcliente;
+  public token;
+
+  public carrito_arr : Array<any> = [];
+  public url;
+  public subtotal = 0;
+  public total_pagar = 0;
+  public socket = io('http://localhost:4201');
+
+  constructor(
+    private _clienteService: ClienteService,
+  ) { 
+
+    this.idcliente = localStorage.getItem('_id');
+    this.token = localStorage.getItem('token');
+    this.url =GLOBAL.url;
+
+
+    this._clienteService.obtener_carrito_cliente(this.idcliente,this.token).subscribe(
+      response=>{
+        this.carrito_arr = response.data;
+        this.calcular_carrito();
+        
+      }
+    );
+  }
+
+  ngOnInit(): void {
+  }
+
+  calcular_carrito(){
+    this.carrito_arr.forEach((element: any) => {
+ 
+      this.subtotal = Math.round(element.product.price * element.quantity);
+     
+      this.total_pagar=(this.total_pagar+this.subtotal/this.carrito_arr.length)
+      
+    });
+    this.total_pagar = this.subtotal;
+  }
+
+  eliminar_item(id:any){
+    this._clienteService.eliminar_carrito_cliente(id,this.token).subscribe(
+      response=>{
+          iziToast.show({
+            title: 'SUCCESS',
+            titleColor: '#1DC74C',
+            color: '#FFF',
+            class: 'text-success',
+            position: 'topRight',
+            message: 'Se eliminó el producto correctamente.'
+        });
+        this.socket.emit('delete-carrito',{data:response.data});
+        this._clienteService.obtener_carrito_cliente(this.idcliente,this.token).subscribe(
+          response=>{
+            this.carrito_arr = response.data;
+            this.calcular_carrito();
+            
+          }
+        );
+        
+      }
+    );
+  }
+
+}
